1d16cf0544b75dba4a6981c07081cf5d62e8c6c4
Damien Wuterich <damienwuterich@gmail.com>
2023-02-26 21:11:05 -0800

    Follow cursor with circle

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 document.onmousemove = function (event) {
+  const x = event.clientX;
+  const y = event.clientY;
+
   const output = document.getElementById("cursor-pos");
-  output.innerHTML = `${event.clientX},${event.clientY}`;
+  output.innerHTML = `${x},${y}`;
+
+  const circle = document.getElementById("circle");
+  circle.style.left = x;
+  circle.style.top = y;
 };
